960998476499fa6aff99984c997185ca51393a24
uomma <marcelloleone975@gmail.com>
2023-02-24 19:57:06 +0100

    funziona anche alert ma finisce tutto array

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,33 @@
 'use:script'
+ const mail = prompt('enter your mail');
+ console.log(mail);
+
+/*const test = true;
+ console.log(test); */
+
+ const operatore =["gmail", "libero", "alice", "hotmail", "outlook"];
+ console.log(operatore);
+ const accessoConsentito = 'accesso consentito';
+ const accessoNegato = 'accesso negato';
+
+
+//for(let i = 0; i < operatore.length; i++)
+//{
+    /* cosi mi stampa tutto ciò che è dentro array:operatore */
+    //console.log(operatore[i])    
+
+//}//
+
+for(let i = 0; i < operatore.length; i++){
+    if(operatore[i]===mail){
+        console.log(true);
+        alert(accessoConsentito);
+    }
+    else if(operatore !== mail){
+            console.log(false);
+            alert(accessoNegato);
+          
+    }
+    else{break}
+}
+

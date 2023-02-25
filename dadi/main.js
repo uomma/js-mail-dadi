82a2cbe805d1d61439aeda0e7897cfd952a011a6
uomma <marcelloleone975@gmail.com>
2023-02-25 01:32:51 +0100

    dadi.done

--- dadi/main.js
+++ dadi/main.js
@@ -1,2 +1,19 @@
 'use:script'
 
+const numeroUser = Math.floor(Math.random()*6 + 1);
+const numeroCpu = Math.floor(Math.random()*6 + 1);
+let text = '';
+
+console.log('Numero User: ' + numeroUser);
+console.log('Numero CPU: ' + numeroCpu);
+
+if(numeroUser > numeroCpu){
+    text = 'Hai vinto';
+}else if(numeroUser < numeroCpu){
+    text = 'Hai Perso';
+}else{
+    text = 'Pareggio';
+}
+
+
+console.log(text);
